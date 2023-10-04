0227d3389a98113517b4379e021a15c9ba8f50dd
Simone <simoneramundi@outlook.com>
2023-10-04 15:05:25 +0200

    Formatting currency using pipes.

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -6,14 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name: string = '';
-  date: string = '';
-
+  name!: string;
+  date!: string;
+  amount!: number;
 
   onNameChange(value:string){
     this.name = value;
   }
   onDateChange(value:string){
     this.date = value;
+  }
+
+  onAmountChange(value:string){
+    this.amount = parseFloat(value);
   }
 }
